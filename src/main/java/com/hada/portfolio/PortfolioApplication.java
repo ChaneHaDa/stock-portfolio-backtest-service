5769f342aa5a62bf6d2c869480d288b28094edb4
ChaneHaDa <chane.ha.da@gmail.com>
2023-11-30 19:21:47 +0900

    initial commit

---
+++ src/main/java/com/hada/portfolio/PortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.hada.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioApplication {
+
+	public static main(String[] args) {
+		SpringApplication.run(PortfolioApplication.class, args);
+	}
+
+}
