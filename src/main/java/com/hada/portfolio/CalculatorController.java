3f72428bfa7d56a49ab41749816179d7babebd9a
ChaneHaDa <chane.ha.da@gmail.com>
2024-01-06 20:41:06 +0900

    feat: add welfare calculator

--- src/main/java/com/hada/portfolio/CalculatorController.java
+++ src/main/java/com/hada/portfolio/CalculatorController.java
@@ -15,13 +15,48 @@ public class CalculatorController {
     }
 
     @GetMapping("/ror")
-    public String getRor(Model model, @RequestParam double buyPrice, @RequestParam double sellPrice, @RequestParam long quantity) {
-        model.addAttribute("ror", (sellPrice - buyPrice) / (double) buyPrice * 100);
-        model.addAttribute("profit", (long) ((sellPrice - buyPrice) / (double) buyPrice * 100 * quantity));
-        model.addAttribute("buyPrice", buyPrice);
-        model.addAttribute("sellPrice", sellPrice);
-        model.addAttribute("quantity", quantity);
+    public String getRor(Model model,
+                         @RequestParam(defaultValue = "0.0") String buyPrice,
+                         @RequestParam(defaultValue = "0.0") String sellPrice,
+                         @RequestParam(defaultValue = "0") String quantity) {
+        try {
+            double buyPriceDouble = Double.parseDouble(buyPrice);
+            double sellPriceDouble = Double.parseDouble(sellPrice);
+            long quantityLong = Long.parseLong(quantity);
 
+            // 사용자가 제대로된 숫자값을 입력하지 않았을 경우를 처리할 수 있습니다.
+            // buyPriceDouble, sellPriceDouble, quantityLong를 사용하여 작업을 수행합니다.
+
+            model.addAttribute("ror", (sellPriceDouble - buyPriceDouble) / buyPriceDouble * 100);
+            model.addAttribute("profit", (long) ((sellPriceDouble - buyPriceDouble) * quantityLong));
+            model.addAttribute("buyPrice", buyPriceDouble);
+            model.addAttribute("sellPrice", sellPriceDouble);
+            model.addAttribute("quantity", quantityLong);
+        } catch (NumberFormatException e) {
+            // 잘못된 입력 형식에 대한 처리
+            return "calculator";
+        }
+        return "calculator";
+    }
+
+    @GetMapping("/welfare")
+    public String getWelfare(Model model,
+                         @RequestParam(defaultValue = "0.0") String price,
+                         @RequestParam(defaultValue = "0") String term,
+                         @RequestParam(defaultValue = "0.0") String welfareRor) {
+        try {
+            double priceDouble = Double.parseDouble(price);
+            long termLong = Long.parseLong(term);
+            double welfareRorDouble = Double.parseDouble(welfareRor);
+
+            model.addAttribute("price", price);
+            model.addAttribute("term", term);
+            model.addAttribute("welfareRor", welfareRor);
+
+        } catch (NumberFormatException e) {
+            // 잘못된 입력 형식에 대한 처리
+            return "calculator";
+        }
         return "calculator";
     }
 }
