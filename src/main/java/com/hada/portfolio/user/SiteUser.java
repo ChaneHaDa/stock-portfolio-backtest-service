fead6d2dfe381f24a38ec7fecb659830cbf5f75c
ChaneHaDa <chane.ha.da@gmail.com>
2024-01-18 15:07:44 +0900

    feat: add security, signup, login, logout

---
+++ src/main/java/com/hada/portfolio/user/SiteUser.java
@@ -0,0 +1,64 @@
+package com.hada.portfolio.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity
+@Getter
+@Setter
+public class SiteUser implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    private String authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        ArrayList<GrantedAuthority> auth = new ArrayList<GrantedAuthority>();
+        auth.add(new SimpleGrantedAuthority(authorities));
+
+        return auth;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
---
+++ src/main/java/com/hada/portfolio/user/UserController.java
@@ -0,0 +1,52 @@
+package com.hada.portfolio.user;
+
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private final SiteUserService siteUserService;
+
+    public UserController(SiteUserService siteUserService){
+        this.siteUserService = siteUserService;
+    }
+
+    @GetMapping("/signup")
+    public String signup(UserCreateForm userCreateForm) {
+        return "signup_form";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "signup_form";
+        }
+
+        if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
+            bindingResult.rejectValue("password2", "passwordInCorrect",
+                    "2개의 패스워드가 일치하지 않습니다.");
+            return "signup_form";
+        }
+
+        if (siteUserService.findByUsername(userCreateForm.getUsername())){
+            bindingResult.rejectValue("username", "usernameDuplicated",
+                    "이미 사용중인 사용자ID입니다.");
+            return "signup_form";
+        }
+
+        siteUserService.save(userCreateForm.getUsername(), userCreateForm.getPassword1());
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login_form";
+    }
+}
