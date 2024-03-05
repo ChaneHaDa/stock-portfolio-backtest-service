89ff4a53c712d74330cfae659e3cbf3f64f795ac
ChaneHaDa <chane.ha.da@gmail.com>
2024-03-05 14:49:44 +0900

    style: change package name

--- src/main/java/com/hada/portfolio/user/SiteUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hada.portfolio.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface SiteUserRepository extends JpaRepository<SiteUser, Long> {
-    Optional<SiteUser> findByUsername(String username);
-}
